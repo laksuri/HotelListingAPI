25bb6bc6afeda3a036b4532bd29496bfa8279a28
Surya V <laksuri.v@gmail.com>
2023-12-22 04:08:40 -0600

    Seeding Data

--- HotelListingAPI/Data/HotelDbContext.cs
+++ HotelListingAPI/Data/HotelDbContext.cs
@@ -9,5 +9,45 @@ namespace HotelListingAPI.Data
         }
         public DbSet<Hotel> Hotels { get; set; }
         public DbSet<Country> Countries { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+            modelBuilder.Entity<Country>().HasData(
+                new Country
+                {
+                    Id = 1,
+                    Name = "India",
+                    ShortName = "IND"
+                },
+                new Country
+                {
+                    Id = 2,
+                    Name = "Dubai",
+                    ShortName = "DXB"
+                },
+                new Country
+                { 
+                    Id = 3,
+                    Name = "Singapore",
+                    ShortName = "SIN"
+                }
+                );
+            modelBuilder.Entity<Hotel>().HasData(
+                new Hotel
+                {
+                    Id = 1,
+                    Name = "Lemon Tree",
+                    Address = "Hyderabad",
+                    CountryId = 1
+                },
+                new Hotel
+                {
+                    Id=2,
+                    Name="Taj Dubai",
+                    Address="Dubai",
+                    CountryId=2
+                });
+        }
     }
 }
---
+++ HotelListingAPI/Migrations/20231222100146_SeedData.cs
@@ -0,0 +1,64 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+#pragma warning disable CA1814 // Prefer jagged arrays over multidimensional
+
+namespace HotelListingAPI.Migrations
+{
+    /// <inheritdoc />
+    public partial class SeedData : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.InsertData(
+                table: "Countries",
+                columns: new[] { "Id", "Name", "ShortName" },
+                values: new object[,]
+                {
+                    { 1, "India", "IND" },
+                    { 2, "Dubai", "DXB" },
+                    { 3, "Singapore", "SIN" }
+                });
+
+            migrationBuilder.InsertData(
+                table: "Hotels",
+                columns: new[] { "Id", "Address", "CountryId", "Name", "Rating" },
+                values: new object[,]
+                {
+                    { 1, "Hyderabad", 1, "Lemon Tree", 0.0 },
+                    { 2, "Dubai", 2, "Taj Dubai", 0.0 }
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "Countries",
+                keyColumn: "Id",
+                keyValue: 3);
+
+            migrationBuilder.DeleteData(
+                table: "Hotels",
+                keyColumn: "Id",
+                keyValue: 1);
+
+            migrationBuilder.DeleteData(
+                table: "Hotels",
+                keyColumn: "Id",
+                keyValue: 2);
+
+            migrationBuilder.DeleteData(
+                table: "Countries",
+                keyColumn: "Id",
+                keyValue: 1);
+
+            migrationBuilder.DeleteData(
+                table: "Countries",
+                keyColumn: "Id",
+                keyValue: 2);
+        }
+    }
+}
